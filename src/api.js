e0bb2c9283b956a35c759539f6e5fad4f52bb103
Basile Vernouillet <basile64.v@gmail.com>
2020-12-21 19:03:09 +0100

    player is now working

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://api-bazify.basile.vernouillet.dev"
-    : "http://localhost:8000";
+    : "http://localhost:5000";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="p-grid p-jc-between">
-    <form class="p-col-12 p-offset-0 p-md-6 p-md-offset-3">
+  <div class="p-grid nested-grid">
+    <form class="p-col-10 p-offset-1 p-md-8 p-md-offset-2">
       <FileUpload
-        name="files"
-        :url="`${API_URL}/songs`"
+        name="songs"
+        :url="`${API_URL}/api/v1/songs`"
         :multiple="true"
         @upload="onUpload"
       >
@@ -12,39 +12,118 @@
         </template>
       </FileUpload>
     </form>
-    <figure v-for="song in songs" :key="song.id">
-      <figcaption>
-        {{ song.title }}
-      </figcaption>
-      <audio controls preload="none">
-        <source :src="song.url" type="audio/mpeg" />
-      </audio>
-    </figure>
+
+    <section class="p-col-6 p-offset-3">
+      <h2>
+        {{ currentSong.title }}
+      </h2>
+      <div class="p-grid">
+        <div class="p-col-4">
+          <Button
+            icon="pi pi-backward"
+            class="p-button-rounded"
+            @click="prev"
+          />
+        </div>
+        <div v-if="!isPlaying" class="p-col-4 text-center">
+          <Button
+            icon="pi pi-play"
+            class="p-button-rounded"
+            @click="play(null, currentSong)"
+          />
+        </div>
+        <div v-else class="p-col-4 text-center">
+          <Button icon="pi pi-pause" class="p-button-rounded" @click="pause" />
+        </div>
+        <div class="p-col-4 text-center">
+          <Button icon="pi pi-forward" class="p-button-rounded" @click="next" />
+        </div>
+      </div>
+    </section>
+    <section class="p-col-6 p-offset-3">
+      <div class="p-grid">
+        <h3 class="p-col-12">The Playlist</h3>
+        <Button
+          v-for="song in songs"
+          :key="song.src"
+          @click="play(null, song)"
+          class="p-col-12"
+          :class="song.id === currentSong.id ? 'song p-button-danger' : 'song'"
+          :label="song.title"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import { onMounted, ref } from "vue";
 import FileUpload from "primevue/fileupload";
+import Button from "primevue/button";
 
 import { API_URL } from "../api";
 
 export default {
   name: "Home",
   components: {
-    FileUpload
+    FileUpload,
+    Button
   },
   setup: () => {
+    const currentSong = ref({});
+    const index = ref(0);
+    const isPlaying = ref(false);
     const songs = ref([]);
+    const player = ref(new Audio());
+
+    const play = (_, song) => {
+      if (typeof song.src != undefined) {
+        currentSong.value = song;
+
+        player.value.src = currentSong.value.path;
+      }
+      player.value.play();
+      player.value.addEventListener("ended", () => {
+        console.log("ended");
+        index.value++;
+        if (index.value > songs.value.length - 1) {
+          index.value = 0;
+        }
+      });
+      isPlaying.value = true;
+    };
+
+    const pause = () => {
+      player.value.pause();
+      isPlaying.value = false;
+    };
+
+    const next = () => {
+      index.value++;
+      if (index.value > songs.value.length - 1) {
+        index.value = 0;
+      }
+      currentSong.value = songs.value[index.value];
+      play(null, currentSong.value);
+    };
+
+    const prev = () => {
+      index.value--;
+      if (index.value < 0) {
+        index.value = songs.value.length - 1;
+      }
+      currentSong.value = songs.value[index.value];
+      play(null, currentSong.value);
+    };
 
     const fetchSongs = async () => {
-      const res = await fetch(API_URL + "/songs");
+      const res = await fetch(API_URL + "/api/v1/songs");
       const json = await res.json();
 
       songs.value = json.map(s => {
         return {
           ...s,
-          url: API_URL + s.url
+          path: API_URL + s.path
         };
       });
     };
@@ -53,13 +132,30 @@ export default {
       await fetchSongs();
     };
 
-    onMounted(fetchSongs);
+    onMounted(async () => {
+      await fetchSongs();
+      currentSong.value = songs.value[index.value];
+      player.value.src = currentSong.value.path;
+    });
 
     return {
       songs,
+      currentSong,
       onUpload,
-      API_URL
+      API_URL,
+      player,
+      isPlaying,
+      play,
+      pause,
+      prev,
+      next
     };
   }
 };
 </script>
+
+<style>
+text-center {
+  text-align: center;
+}
+</style>
